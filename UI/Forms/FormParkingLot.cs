3f42aec6536fdd928ca2db65b513f1f87ba85c8f
GerdaBoman <88060352+GerdaBoman@users.noreply.github.com>
2022-03-23 11:12:58 +0100

    mc checkout fix

--- UI/Forms/FormParkingLot.cs
+++ UI/Forms/FormParkingLot.cs
@@ -229,7 +229,7 @@ public partial class FormParkingLot : Form
 
                     SpotsStatus(spot, myButton);
                 }
-                else if (vehicleType == "Mc")
+                else if (vehicleType == "MC")
                 {
                     price = Math.Round((double)timeParked.TotalHours * config.McFeePerHour, 2); // CHANGES ACCORDING TO JSON
 
